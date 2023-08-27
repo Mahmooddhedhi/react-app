792e16969da0212c4a53f138c8e3b25ea2c74a77
Muhammad mehmood <106702969+Mahmooddhedhi@users.noreply.github.com>
2023-08-28 03:22:25 +0500

    Add files via upload

---
+++ store.js
@@ -0,0 +1,14 @@
+// import {  applyMiddleware } from 'redux'; // Use the correct import for createStore
+// import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import rootReducers from './src/components/redux/reducers/Main'; // Assuming this is the correct path to your reducers
+
+// const middleware = [thunk];
+
+// const store = configureStore(
+//   rootReducers,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+// export default store; // Changed variable name to "store" for consistency
